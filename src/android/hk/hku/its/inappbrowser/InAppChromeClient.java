4c4f65f29f78aad74b4f805029fd53fe526e5c87
Quentin Fung <31120177+QuentinFung@users.noreply.github.com>
2018-04-17 09:59:04 +0800

    change to use ITS theme

--- src/android/hk/hku/its/inappbrowser/InAppChromeClient.java
+++ src/android/hk/hku/its/inappbrowser/InAppChromeClient.java
@@ -1,22 +1,4 @@
-/*
-       Licensed to the Apache Software Foundation (ASF) under one
-       or more contributor license agreements.  See the NOTICE file
-       distributed with this work for additional information
-       regarding copyright ownership.  The ASF licenses this file
-       to you under the Apache License, Version 2.0 (the
-       "License"); you may not use this file except in compliance
-       with the License.  You may obtain a copy of the License at
-
-         http://www.apache.org/licenses/LICENSE-2.0
-
-       Unless required by applicable law or agreed to in writing,
-       software distributed under the License is distributed on an
-       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-       KIND, either express or implied.  See the License for the
-       specific language governing permissions and limitations
-       under the License.
-*/
-package org.apache.cordova.inappbrowser;
+package hk.hku.its.inappbrowser;
 
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.LOG;
@@ -122,7 +104,7 @@ public class InAppChromeClient extends WebChromeClient {
             else
             {
                 // Anything else with a gap: prefix should get this message
-                LOG.w(LOG_TAG, "InAppBrowser does not support Cordova API calls: " + url + " " + defaultValue); 
+                LOG.w(LOG_TAG, "InAppBrowser does not support Cordova API calls: " + url + " " + defaultValue);
                 result.cancel();
                 return true;
             }
